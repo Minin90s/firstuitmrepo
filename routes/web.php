98a6df971db7e9770910493d8387739d3347b19a
Amirul Mukminin <2019314445@isiswa.uitm.edu.my>
2019-11-09 11:47:46 +0800

    make authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testing', function(){
+    echo "This is testing";
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
