af0dc2680685d6d9d8c4db0a263db2a6a37c2dd9
Amirul Mukminin <2019314445@isiswa.uitm.edu.my>
2019-11-09 14:25:08 +0800

    Third commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Amirul',
+            'email' => 'admin@gmail.my',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
